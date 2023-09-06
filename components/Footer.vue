366b36bef4b3280fb5c9f6d86182bd1e18a314f5
Abdul Majid Refindo <abdulmajirefindo@gmail.com>
2023-09-06 14:17:36 +0700

    layouting footer

---
+++ components/Footer.vue
@@ -0,0 +1,127 @@
+<template>
+    <footer class="w-100 py-4 flex-shrink-0">
+        <div class="container py-4">
+            <div class="row gy-4 gx-5">
+                <div class="col-lg-5 col-md-6">
+                    <h5 class="text-white mb-3">Lokasi</h5>
+                    <p id="address" class="text-white mb-0">Jl. Jatinegara Barat 1 No.5-6, RT.2/RW.4, Bali Mester,</p>
+                    <p id="address" class="text-white mb-0">Kecamatan Jatinegara, Kota Jakarta Timur,</p>
+                    <p id="address" class="text-white mb-3">Daerah Khusus Ibukota Jakarta 13310</p>
+                    <div id="dummy_map"></div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-3">Follow us</h5>
+                    <h5 class="text-white mb-1">Smartnation.id</h5>
+                    <p id="address" class="text-white mb-2">Toward Indonesia Smart Nation</p>
+                    <div class="row mb-0 mt-0">
+                        <div class="col">
+                            <div id="dummy_ig"></div>
+                        </div>
+                        <div class="col">
+                            <div id="dummy_ig"></div>
+                        </div>
+                        <div class="col">
+                            <div id="dummy_ig"></div>
+                        </div>
+                    </div>
+                    <button type="button" class="btn btn-danger mt-3">
+                        <div class="row align-items-center">
+                            <div class="col-1"><img style="width:18px" src="../public/icon-ig.png" alt=""></div>
+                            <div id="text-button" class="col-10">Follow on Instagram</div>
+                        </div>
+                    </button>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="text-white mb-4">Populer Tags    </h5>
+                    <div class="row">
+                        <div class="col">
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                        </div>
+                        <div class="col">
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                        </div>
+                        <div class="col">
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                            <p class="small text-white">Lorem</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+footer
+{
+    display: flex;
+    background-color: #262626; 
+    height: 360px;
+    width: 100%;
+    position: fixed;
+    font-family: Poppins;
+}
+
+#address
+{
+    color: #E7E7E7;
+    font-family: Poppins;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 100px;
+    line-height: 150%;
+}
+
+#dummy_map
+{
+    background-color: #F6F6F6;
+    height: 140px;
+    width: 500px; 
+    z-index: 1;
+    border-radius: 10px;
+}
+
+#dummy_ig
+{
+    background-color: #F6F6F6;
+    height: 80px;
+    width: 80px; 
+    z-index: 1;
+    border-radius: 8px;
+    margin-top: 3px;
+    margin-bottom: 3px;
+}
+
+p
+{
+    font-size: 12.5px;
+    margin-bottom: 10px;
+    font-style: normal;
+    font-weight: 300;
+    color: #F6F6F6;
+}
+
+#text-button
+{
+    font-size: 13px;
+    color: #FFF;
+    text-align: center;
+}
+
+button
+{
+    border-radius: 6px;
+    background: var(--primary-600, #D71149);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,5 @@
 <template>
-  <div>
-    <div class="container py-3">
-      <h1 class="text-danger text-capitalize">Halaman Utama </h1>
-      <NuxtLink to="/about"
-        class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-danger">
-        Lanjutkan ke halaman Tentang Kami
-        <BootstrapIcon name="arrow-right" />
-      </NuxtLink>
 
-      <div class="px-3 mx-3 bg-white shadow-sm rounded">
-        <h1 class="text-start text-capitalize fst-normal fs-3 pt-3">Daftar Berita</h1>
-        <hr class="text-dark" />
-        <div v-show="isLoading" class="text-start text-dark fst-normal fs-6 mb-3">Sedang memuat...</div>
-
-        <ul class="list-group list-group-flush">
-          <li v-for="(article, index ) in articles?.sort((a, b) => {
-            return b.title.localeCompare(a.title)
-          })" :key="index" class="list-group-item px-0 mx-0">
-
-            <!-- <NuxtImg :src="article.cover" :width="250" :height="250" :alt="article.title" loading="lazy"
-              class="img-fluid rounded" /> -->
-            <h3 class="text-start text-dark-50 fs-5 fst-normal">{{ article.title }}</h3>
-            <span v-for="category in article.category" :key="category">{{ category }}</span>
-
-            <div v-html="article.content.substring(0, 250)"></div>
-          </li>
-        </ul>
-
-      </div>
-
-    </div>
-
-
-
-  </div>
 </template>
 
 <script setup>
