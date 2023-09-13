23bf6d0fb3b9c69e27211f71e4d84e3472ce5475
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2023-09-13 09:24:37 +0700

    Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,6 @@ export default defineNuxtConfig({
     '@nuxtjs/google-fonts',
     '@nuxt/image',
     '@nuxtjs/robots',
-    '@nuxtjs/color-mode',
     'nuxt-bootstrap-icons',
     'nuxt-swiper',
   ],
@@ -28,12 +27,6 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       titleTemplate: '%s - Smartnation',
-      script: [
-        {async:true, src: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"},
-        {async:true, src: "https://cdnjs.cloudflare.com/ajax/libs/leaflet-ajax/2.1.0/leaflet.ajax.min.js"},
-        {async:true, src: "https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.js"},
-        {async:true, src: "https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw-src.js"},
-      ]
     },
   },
 
@@ -48,20 +41,7 @@ export default defineNuxtConfig({
   },
 
   googleFonts: {
-    families: {
-      Poppins: [300, 400, 500, 600],
-    },
+    families: { Poppins: [300, 400, 500, 600] },
     // options...
-  },
-
-  colorMode: {
-    preference: 'system', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '-mode',
-    storageKey: 'nuxt-color-mode',
   },
 });
