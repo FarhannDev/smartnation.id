72efa3522e1bbbc86b57d278a315094907082852
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2023-11-09 10:58:10 +0700

    fixing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -138,7 +138,7 @@ export default defineNuxtConfig({
 
   // Nuxt.js Color Mode configuration
   colorMode: {
-    preference: 'system', // default value of $colorMode.preference
+    preference: 'light', // default value of $colorMode.preference
     fallback: 'light', // fallback value if not system preference found
     hid: 'nuxt-color-mode-script',
     globalName: '__NUXT_CCSN_COLOR_MODE__',
