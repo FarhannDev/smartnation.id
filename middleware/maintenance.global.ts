31385d186dde470748aa877b5a50416f222d1391
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2023-12-27 17:01:04 +0700

    Update Maintenance

--- middleware/maintenance.global.ts
+++ middleware/maintenance.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   // Contoh kondisi jika situs dalam mode maintenance
-  const maintenanceMode = true;
+  const maintenanceMode = false;
 
   if (import.meta.env.PROD && maintenanceMode && to.path !== '/maintenance') {
     return navigateTo('/maintenance', { redirectCode: 503 });
